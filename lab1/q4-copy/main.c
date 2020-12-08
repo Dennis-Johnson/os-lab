56313032c62cd516553fda15a29ec065f75251ec
Dennis Johnson <densarjohn@gmail.com>
2020-12-09 00:01:35 +0530

    Fixed q4

--- lab1/q4-copy/main.c
+++ lab1/q4-copy/main.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+/*
+ * Program to copy char by char from src to dst file
+ * */
 
 int main(int argc, char* argv[]){
   if(argc != 3){
@@ -7,22 +15,26 @@ int main(int argc, char* argv[]){
     exit(EXIT_FAILURE);
   }
 
-  FILE *fin, *fout;
-  if ((fin = fopen(argv[1], "r")) == NULL){
+  int ch, in, out;
+
+  if ((in = open(argv[1], O_RDONLY)) < 0){
     fprintf(stderr, "Error opening src file %s", argv[1]);
     perror(" ");
     exit(EXIT_FAILURE);
   }
   
-  if ((fout = fopen(argv[2], "w")) == NULL){
+  if ((out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IXOTH)) < 0){
     fprintf(stderr, "Error opening dst file %s", argv[2]);
     perror(" ");
     exit(EXIT_FAILURE);
   }
   
-  int ch;
-  while((ch = fgetc(fin)) != EOF){
-    fputc(ch, fout);
+  size_t len_read, len_write;
+  while((len_read = read(in, &ch, 1)) > 0){
+    if ((len_write = write(out, &ch, 1)) < 0){
+      fprintf(stderr, "Error writing to dst file");
+      perror(" ");
+    }
   }
 
   printf("Done copying!\n");
