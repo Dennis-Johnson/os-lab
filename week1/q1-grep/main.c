93a82f7c80bd29ee9b61ad70b37ec4d241a2b2a0
Dennis Johnson <densarjohn@gmail.com>
2020-12-08 22:34:13 +0530

    lab1 q1

---
+++ week1/q1-grep/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define LEN_BUFFER 128
+
+int main(int argc, char* argv[]){
+  FILE *fin;
+  char buffer[LEN_BUFFER];
+
+  if(argc != 3){
+    fprintf(stderr, "Usage: arg1 - word, arg2 - file src to search in");  
+  }
+  
+  fin = fopen(argv[2], "r");
+  if(fin == NULL){
+    fprintf(stderr, "Error opening file %s", argv[2]);
+    perror(" ");
+  }
+  
+  while(fgets(buffer, LEN_BUFFER, fin) != NULL){
+    if(strstr(buffer, argv[1]) == NULL)
+      continue;
+    
+    printf("Found '%s': line --> %s", argv[1], buffer);
+  }
+
+	return 0;
+}
