94bf4642ea0e34250309b9a949c9ce23088a6937
Dennis Johnson <densarjohn@gmail.com>
2020-12-09 00:03:28 +0530

    Q3 needs to be redone

---
+++ lab1/q3-conv-spec/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+int main()
+{
+	printf("%d \n",0xf);
+	printf("%s\n","hello" );
+	printf("%e\n",3.14);
+	printf("%o\n",12);
+	printf("%f\n",0.1 );
+	printf("%#x\n",1 );
+	open("hel",O_RDONLY);
+	fprintf(stderr,"%m\n");
+	
+} 
