7a208bba82f6cdfd8ce30cbf2c38b3ca70ccbd07
Dennis Johnson <densarjohn@gmail.com>
2020-12-09 12:23:40 +0530

    Q1 incomplete

--- lab1/q1-grep/main.c
+++ lab1/q1-grep/main.c
@@ -1,23 +1,37 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stats.h>
+#include<fctnl.h>
 #include<string.h>
 #include<stdlib.h>
-#define LEN_BUFFER 128
+
+#define LEN_BUFFER 256
+/*
+ * Search for a word in file given as cmd line ard
+ * Similar to grep for a single file
+ * */
 
 int main(int argc, char* argv[]){
-  FILE *fin;
-  char buffer[LEN_BUFFER];
+  int in;
+  char ch, buffer[LEN_BUFFER];
 
   if(argc != 3){
     fprintf(stderr, "Usage: arg1 - word, arg2 - file src to search in");  
   }
   
-  fin = fopen(argv[2], "r");
-  if(fin == NULL){
+  if((in = open(argv[2], O_RDONLY)) < 0){
     fprintf(stderr, "Error opening file %s", argv[2]);
     perror(" ");
+    exit(EXIT_FAILURE);
   }
   
-  while(fgets(buffer, LEN_BUFFER, fin) != NULL){
+  size_t len_read;
+  while((len_read = read(in, &ch, sizeof(char))) > 0){
+    if (ch == '\n'){
+      //Do stuff
+    }
+    buffer[char_count++] = ch;
     if(strstr(buffer, argv[1]) == NULL)
       continue;
     
