0e56417413032e4baa8f74120a0ad1a711c89165
Dennis Johnson <densarjohn@gmail.com>
2020-12-22 20:02:23 +0530

    Fixes

--- lab8/q4-evenodd/main.c
+++ lab8/q4-evenodd/main.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 
 #define ARR_LEN 6
+
 int arr[ARR_LEN] = {
     1,
     2,
@@ -10,37 +11,32 @@ int arr[ARR_LEN] = {
     4,
     5,
     6};
+int even_sum = 0, odd_sum = 0;
 
 void *sum_even(void *param)
 {
-  int even_sum = 0;
   for (int i = 0; i < ARR_LEN; i++)
   {
     if (arr[i] % 2 == 0)
       even_sum += arr[i];
   }
 
-  printf("\nEven sum: %d\n", even_sum);
-  return (void *)even_sum;
+  pthread_exit(NULL);
 }
 
 void *sum_odd(void *param)
 {
-  int odd_sum = 0;
   for (int j = 0; j < ARR_LEN; j++)
   {
     if (arr[j] % 2 != 0)
       odd_sum += arr[j];
   }
 
-  printf("\nOdd sum: %d\n", odd_sum);
-  return (void *)odd_sum;
+  pthread_exit(NULL);
 }
 
 int main()
 {
-  int even_sum = 0, odd_sum = 0;
-
   printf("Array contents: ");
   for (int i = 0; i < ARR_LEN; i++)
   {
@@ -51,10 +47,10 @@ int main()
   pthread_create(&even_adder, NULL, sum_even, NULL);
   pthread_create(&odd_adder, NULL, sum_odd, NULL);
 
-  pthread_join(even_adder, (void **)&even_sum);
-  pthread_join(odd_adder, (void **)&odd_sum);
+  pthread_join(even_adder, NULL);
+  pthread_join(odd_adder, NULL);
 
-  printf("\n\nEven sum: %d\nOdd Sum: %d\n Total: %d\n", (int)even_sum, (int)odd_sum, even_sum + odd_sum);
+  printf("\n\nEven sum: %d\nOdd Sum: %d\nTotal: %d\n", (int)even_sum, (int)odd_sum, even_sum + odd_sum);
 
   return 0;
 }
